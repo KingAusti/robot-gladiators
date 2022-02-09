100e1e8a96061d5e23928898ec5f137dbc722f5a
KingAusti <austin@thehenrys.xyz>
2022-02-08 22:20:10 -0800

    initial game setup

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+function fight() {
+    window.alert("the fight has begun!");
+}
+// fight();
+
+var playerName = window.prompt("what is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a note");
+console.log(10+10);
+console.log("It is very nice to meet you, " + playerName);
